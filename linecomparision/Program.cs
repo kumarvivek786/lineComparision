3f4d2980897b70b228d18fc34114757c154e8ca2
vivek <ikumarvivek786@gmail.com>
2022-09-18 22:17:45 +0530

    CalculateLengthUC-1

---
+++ linecomparision/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linecomparision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC-1 Calculate the Length of Line
+            //variables
+            int x1;
+            int y1;
+            int x2;
+            int y2;
+            double Length;
+
+            
+            Console.WriteLine("x1 value:");
+            x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y1 value:");
+            y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("x2 value:");
+            x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y2 value:");
+            y2 = Convert.ToInt32(Console.ReadLine());
+
+            //Formula
+            Length = Math.Sqrt((x2 - x1 ^ 2 + y2 - y1 ^ 2));
+
+            Console.WriteLine("Length of the Line is: " + Length);
+            Console.ReadLine();
+
+        }
+    }
+}
